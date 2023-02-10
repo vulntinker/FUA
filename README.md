29fff6d0b7ac24f19cfb38d4b9a515fa42130ce8
vulntinker <47965030+vulntinker@users.noreply.github.com>
2023-02-10 17:19:53 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# FUA
-WEB API fuzzing 
+# FUA (Fuzzing Unauthorized Api)
+
+# 简述
+
+把JS文件中存在的相对路径用正则表达式匹配出来，然后无脑的用GET和POST去访问，获取返回结果。
+
+项目是功能堆砌差不多后，一边用一边调的，加上本人目前代码水平稀烂，BUG肯定会有，多多包涵。
+
+# 安装
+
+```
+pip install beautifulsoup4 requests termcolor argparse
+```
+
+
+
+# 使用
+
+关于正则，之前的想法是把baseurl: 和 baseapi:作为关键词匹配后面双引号里的路径，但是在几个站点测试之后发现会漏掉不少，所以直接无脑的用正则匹配双引号和单引号内的路径了，所以肯定会有不少垃圾路径数据，目前使用了not in 一个列表和使用了一个黑名单列表简单处理了。
+
+随机生成参数，这里只进行了非常简单的常识，之前的代码里是加了随机数，但是由于垃圾数据的原因会是得有随机数的URL绕过了列表的过滤，所以就写死了数字，同时也没有进行`?id=`类型的参数尝试，还是保持一边用一边写一边调的原则。代码稀烂，各位包涵。
+
+#### -a  参数
+
+可以直接将任意形式的URL丢入-a，会自动获取网页中存在的JS和可能隐藏在JS中的一些JS相对路径。
+
+如果发现JS路径错误，可以手动看一下是否是因为网站JS目录识别错误的问题，如果js前需要拼接一个路径可以使用-k参数
+
+如 http(s)://example.com 中，js存在的路径是分为pc或mobile，那么可以使用
+
+```shell
+python fua.py -a http://example.com -k /pc
+
+python fua.py -a http://example.com -k /mobile
+```
+
+则就相当于访问到了http://example.com/pc 目录下的JS
+
+
+
+程序有自动发现baseURL的功能，但站点的配置各有不同，识别错误或者漏掉肯定会发生，如有问题，可以手动抓个包看一下baseURL的路径，然后使用-b参数，这样就会把-b后的路径拼接到JS中识别到的路径前。
+
+```shell
+假设http://example.com 存在 /log/show_logs 这个接口，
+它的baseurl是/testapi
+程序没有识别到，这个时候使用-b /testapi 就可以把它拼接到所有在JS中匹配到的所有相对路径前。
+```
+
+
+
+#### -j 参数
+
+针对单个JS做相对路径识别，然后无脑请求。
+
+`python fua.py -j http://example.com/0.js`
+
+也可以和-b参数组合使用
+
+`python fua.py -j http://example.com/0.js -b /testapi`
+
+
+
+####  身份信息
+
+如果需要添加登陆之后的身份信息，可以使用 -A 和 -t 参数添加认证信息
+
+![image-20230210162424451](README.assets/image-20230210162424451.png)
+
+
+
+```
+python fua.py -a "http://www.example.com/" -k /pc -b /testapi -A Cookie -t "Session=123456"
+```
+
+
+
+
+
+请求结束后，所有的结果会报存在程序目录下的以URL netloc 为名称的一个txt中。
+
+
+
+# 是否有用
+
+emmm，确实扫到过几个信息泄露和未授权，退一万步，至少可以帮你过滤一下站点在JS中存在的基本接口信息来进一步做一下越权测试。
+
+
+
+如果意愿交流，可以微信或者邮件：
+
+微信: vulntinker
+
+mail: vulntinker@gmail.com
---
+++ fua.py
@@ -0,0 +1,313 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import sys 
+import urllib3
+import random
+from urllib.parse import urlsplit
+from termcolor import colored
+import argparse
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+reg_match = [
+    r'"(/[a-zA-Z0-9_+-]+/[a-zA-Z0-9_+-]+(?:/[a-zA-Z0-9_+-]+)*)"',
+    r'"([a-zA-Z0-9_+-]+/[a-zA-Z0-9_+-]+(?:/[a-zA-Z0-9_+-]+)*)"',
+    r"'(/[a-zA-Z0-9_+-]+/[a-zA-Z0-9_+-]+(?:/[a-zA-Z0-9_+-]+)*)'",
+    r"'([a-zA-Z0-9_+-]+/[a-zA-Z0-9_+-]+(?:/[a-zA-Z0-9_+-]+)*)'",
+]
+# Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36
+
+baseAPI = ""
+baseAPI_list = []
+path_req = []
+req_url = []
+total_js = []
+js_black_list = [
+    "text/plain","+n/100+","text/plain","a/i","DD/MM/YYYY","YYYY/MM/DD","MM/D/YYYY","application/x-www-form-urlencoded","DD/M/YYYY","text/javascript","text/xml","M/D/yy","a/b","image/svg+xml","YYYY/M/D","text/css","D/M/YYYY","MM/DD/YYYY","D/JM"
+]
+
+def make_request(url, data={},auth_type="",token="",num=0,total=0,match_count=0,single_request=False):
+    try:
+        headers = {'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 MicroMessenger/6.6.1 NetType/4G Language/zh_CN'}
+        if token != None and auth_type != None:
+            headers.update({auth_type:token})
+        else:
+            headers = {'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 MicroMessenger/6.6.1 NetType/4G Language/zh_CN'}
+        
+        if single_request:
+            res = requests.get(url=url,headers=headers,verify=False,timeout=60)
+            return res
+
+        response_get = requests.get(url, headers=headers,verify=False,timeout=60)
+        echo_res(url=url,method="GET",res_code=response_get.status_code,res_text=response_get.text,current_num=num,total_num=total,current_regex_num=match_count)
+        
+        response_post = requests.post(url, json=data, headers=headers,verify=False,timeout=60)
+        echo_res(url=url,method="POST",res_code=response_post.status_code,res_text=response_post.text,current_num=num,total_num=total,current_regex_num=match_count)
+        return response_get,response_post
+    except Exception as e:
+        print(colored("[!] Request ERR: "+str(e),"green"))
+        pass
+
+
+
+def echo_res(url,method,res_code,res_text,current_num,total_num,current_regex_num):
+    sys.stdout.write("\033[2K\033[G"+"[+] Regex: {2}, ({0}/{1}) [{3}] URL: {4}".format(current_num,total_num,current_regex_num,method,url))
+    sys.stdout.flush()
+    file = open(urlsplit(url).netloc+".txt", "a")
+    if (res_code == 200 or res_code == 500) and "<html>" not in res_text and "<!doctype html>" not in res_text and "<!DOCTYPE html>" not in res_text and "</script>" not in res_text and 'status":401,' not in res_text and 'status":401}' not in res_text and 'code":401}' not in res_text and 'code":401,' not in res_text and "<title>" not in res_text and 'Code": 404' not in res_text and '"Code": 401' not in res_text and 'status":-1' not in res_text:
+        file.write("\n\n"+url+'\t\t'+str(res_code)+'\t\t'+ method+'\n\n'+res_text+"\n\n\n")
+        print("\n\n")
+        print("URL: ",url)
+        print("Method: ",colored(method, 'red',attrs=["bold"]))
+        print("HTTP CODE: ",res_code)
+        print("SIZE: ",len(res_text))
+        print("RAW: ",colored(res_text, 'red',attrs=["bold"]))
+        print("\n\n")
+    else:
+        file.write(url+'\t\t'+str(res_code)+'\t\t'+ method+'\n\n')
+    file.close()
+
+
+
+def remove_url_params(url):
+    split_url = urlsplit(url)
+    return split_url.scheme + "://" + split_url.netloc.rstrip("/") # 返回http(s)://ip:(ports)的形式
+
+
+
+def find_base_api(url,res_text):
+    global baseAPI
+    global baseAPI_list
+    base_url_match = ['baseurl:"(.*?)"','baseurl: "(.*?)"','baseapi:"(.*?)"','baseapi: "(.*?)"']
+    c = 1
+    if len(baseAPI_list) == 0:
+        for m in base_url_match:
+            baseAPI = re.findall(m, res_text,re.IGNORECASE)
+            if len(baseAPI) == 1 and baseAPI[0] != " " and baseAPI[0] != "":
+                if "http:" in baseAPI[0] or "https:" in baseAPI[0]:
+                    if urlsplit(baseAPI[0]).netloc != urlsplit(url).netloc: # 如何域名不同则设置为输入的域名
+                        baseAPI_list.append(remove_url_params(baseAPI[0]))
+                        return
+                    baseAPI_list.append(baseAPI[0])
+                    return
+                if baseAPI[0][0] != '/' :
+                    baseAPI[0] = '/' + baseAPI[0]
+                baseAPI_list.append(remove_url_params(url)+baseAPI[0])
+                return
+            elif len(baseAPI) == 2 and (baseAPI[1] == "" or baseAPI[1] == " "):
+                if "http://" in baseAPI[0] or "https://" in baseAPI[0]:
+                    baseAPI_list.append(urlsplit(baseAPI[0]).path)
+                    return
+                else:
+                    baseAPI_list.append(remove_url_params(url)+baseAPI[0])
+                    return
+            else: 
+                continue
+        baseAPI_list.append(remove_url_params(url))
+
+
+
+
+def get_apis_from_js_link(js_link,res_text="",user_set_base="",warning=False,token="",auth_type=""):
+    try:     
+        global reg_match
+        global baseAPI
+        global baseAPI_list
+        global path_req
+        global js_black_list
+
+        if warning and user_set_base == "":
+            print(colored("\n\n\t\t [!] baseURL|baseAPI is empty, the outcome of fuzzing may become meaningless.\n\n","green",attrs=["bold"]))
+        if res_text == "":
+            res_text = make_request(url=js_link,token=token,single_request=True,auth_type=auth_type).text
+        if user_set_base != "":
+            baseAPI = user_set_base.replace(" ","")
+            baseAPI_list.append(remove_url_params(js_link)+baseAPI)
+        if baseAPI == "" or "http" in baseAPI or len(baseAPI) == 0:
+            find_base_api(js_link,res_text)
+        if baseAPI_list[0]:
+            print(colored("\n[+] baseAPI|URL: ","red",attrs=["bold"]),baseAPI_list[0])
+
+        print("\n[+] Requesting Apis,this will take a moment...\n")
+
+        match_count = 0
+        guess_url = ""
+        for match in reg_match:
+            count = 0
+            match_count += 1
+            file_path_match = re.findall(match, res_text,re.IGNORECASE)
+            if file_path_match:
+                for js in file_path_match:
+                    if js not in js_black_list:
+                        count += 1
+                        for b in baseAPI_list:
+                            sent = b
+                            if sent[-1] == '/':
+                                sent = sent.rstrip('/')
+                            if js[0] != '/':
+                                js = '/'+js
+                            if "/api" not in js or "api/" not in js:
+                                guess = "/api"+js
+                                guess_url = sent+guess+'/'
+                            if "/logout" in js or "loginOut" in js or "loginout" in js or "resetToken" in js or "refreshToken" in js:
+                                continue 
+                            final_req_url = sent+js
+                        
+                            if final_req_url not in path_req and (all( c.isupper() for c in final_req_url[-5:]) or len(final_req_url) < 120):
+                                path_req.append(final_req_url) 
+
+                                url_with_random_p = final_req_url+'/'+"4321"
+
+                                make_request(url=final_req_url,token=token,auth_type=auth_type,num=count,total=len(file_path_match),match_count=match_count)
+
+                                make_request(url=url_with_random_p,token=token,auth_type=auth_type,num=count,total=len(file_path_match),match_count=match_count)
+
+                                if guess_url not in path_req and (all( c.isupper() for c in guess_url[-5:]) or len(guess_url) < 120 ):
+                                    if guess_url:
+                                        guess_url_with_random_p = guess_url + "1234" # 固定数字方志后面重复访问
+                                        make_request(url=guess_url,token=token,auth_type=auth_type,num=count,total=len(file_path_match),match_count=match_count)
+                                        make_request(url=guess_url_with_random_p,token=token,auth_type=auth_type,num=count,total=len(file_path_match),match_count=match_count)
+    except Exception as e:
+        print("\n")
+        print(colored("[!] *F: get_apis_from_js_link, ERR: "+str(e),"green"))
+        pass
+
+
+
+
+def auto_find_directory(url,token="",auth_type="",user_set_base="",keep_path=""):
+    try:
+        url = url.replace(" ","")
+        global total_js 
+        global reg_match
+        print(colored("[~] May the force be with you ; )\n\n","red",attrs=["bold"]))
+        print("[+] Total regex num:\t",len(reg_match))
+        response = make_request(url=url,token=token,auth_type=auth_type,single_request=True)
+        url = remove_url_params(url) # 访问用户提供的URL之后取netloc
+        if keep_path:
+            url = url + keep_path
+        if response.ok:
+            soup = BeautifulSoup(response.content, "html.parser")
+            tags_and_attrs = [("script", "src"), ("link", "href")]
+            for tag, attr in tags_and_attrs:
+                for js in soup.find_all(tag, {attr: True}):
+                    if js[attr] and (".js" in js[attr]):
+                        js_url = js[attr]
+                        while './' in js_url:
+                            js_url = js_url.replace("./","/")
+                            if '//' in js_url:
+                                js_url = js_url.replace("//","/")
+                        if "://" not in js_url:
+                            if url[-1] != '/':
+                                url += '/'
+                            if js_url[0] == '/':
+                                js_url = js_url.lstrip('/')
+                            if js_url.startswith("./"):
+                                js_url = js_url[2:]
+                            js_url = url + js_url
+                        if urlsplit(js_url).netloc != urlsplit(url).netloc:  # 用来跳过那些第三方的JS，比如JS中调用了高德地图等。
+                                continue
+                        if js_url not in total_js:
+                            total_js.append(js_url)
+            for i in total_js:
+                if "config" in i:
+                    print(colored("\n\n"+"[+] "+i,"yellow"))
+                    js_response = make_request(url=i,auth_type=auth_type,token=token,single_request=True)
+                    find_hiddend_js(i,js_response.text)
+                    get_apis_from_js_link(i,js_response.text,auth_type=auth_type,token=token,user_set_base=user_set_base)
+                    total_js.remove(i)
+                    break
+                elif "app" in i:
+                    print(colored("\n\n"+"[+] "+i,"yellow"))
+                    js_response = make_request(url=i,auth_type=auth_type,token=token,single_request=True)
+                    find_hiddend_js(i,js_response.text)
+                    get_apis_from_js_link(i,js_response.text,auth_type=auth_type,token=token,user_set_base=user_set_base)
+                    total_js.remove(i)
+                    break
+            for j in total_js:
+                print(colored("\n\n"+"[+] "+j,"yellow"))
+                js_response = make_request(url=j,token=token,auth_type=auth_type,single_request=True)
+                find_hiddend_js(j,js_response.text)
+                get_apis_from_js_link(j,js_response.text,token=token,auth_type=auth_type,user_set_base=user_set_base)
+        else:
+            print(colored("[!] ERR:\t"+url+'\t'+str(response.status_code)+"\t"+response.reason,"green")) 
+    except Exception as e:
+        print(colored("[!] *F: auto_find_directory, ERR: "+str(e),"green"))
+        pass
+
+
+def find_hiddend_js(url,res_text):
+    global total_js
+    reg_match = [
+    r'"(/[a-zA-Z0-9_+-.]+/[a-zA-Z0-9_+-.]+(?:/[a-zA-Z0-9_+-.]+)*)"',
+    r'"([a-zA-Z0-9_+-.]+/[a-zA-Z0-9_+-.]+(?:/[a-zA-Z0-9_+-.]+)*)"',
+    r"'(/[a-zA-Z0-9_+-.]+/[a-zA-Z0-9_+-.]+(?:/[a-zA-Z0-9_+-.]+)*)'",
+    r"'([a-zA-Z0-9_+-.]+/[a-zA-Z0-9_+-.]+(?:/[a-zA-Z0-9_+-.]+)*)'",]
+
+    hidden_js = []
+    for i in reg_match:
+        find_hidden_js = re.findall(i, res_text,re.IGNORECASE)
+        if find_hidden_js:
+            for j in find_hidden_js:
+                if j not in hidden_js and ".js" in j:
+                    while "./" in j:
+                        j = j.replace("./","/")
+                        if "//" in j:
+                            j = j.replace("//","/")
+                    if j[0] != '/':
+                        j = '/' + j
+                    domain = remove_url_params(url)
+                    domain = domain+j
+                    if domain not in total_js:
+                        total_js.append(domain)
+
+    
+
+def fuzzing_complete():
+    print("\n")
+    print(colored("[+] Api Fuzzing Completed :)","yellow"))
+
+
+
+if __name__ == '__main__':
+    header = '''
+\t\t\t _______   __    __       ___          
+\t\t\t|   ____| |  |  |  |     /   \         
+\t\t\t|  |__    |  |  |  |    /  ^  \      
+\t\t\t|   __|   |  |  |  |   /  /_\  \       
+\t\t\t|  |      |  `--'  |  /  _____  \  
+\t\t\t|__|       \______/  /__/     \__\ 
+                                    \t\t\t    Fuzzing Unauthorized Api (Beta v1.0)
+                                    \t\t\t    by vulntinker (vulntinker@gmail.com)
+    '''
+    print(colored(header, 'red',attrs=["bold"]))
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-a", "--auto", dest="autourl", help="The site URL that contians JS links.")
+        parser.add_argument("-A", "--auth", dest="auth_type", help="Auth type:AccessToken, Cookie,Authorization")
+        parser.add_argument("-j", "--js", dest="single_js", help="Single JS link to proceed.")
+        parser.add_argument("-b", "--base", dest="url_base", help="baseURL of the site incase the programme missed it.")
+        parser.add_argument("-t", "--token", dest="token", help="token value")
+        parser.add_argument("-k", "--keep", dest="keep_path", help="keep the path of user input URL.")
+
+        args = parser.parse_args()
+        if args.autourl:
+            if args.url_base != None and args.keep_path != None:
+                auto_find_directory(url=args.autourl,auth_type=args.auth_type,token=args.token,user_set_base=args.url_base,keep_path=args.keep_path)
+            if args.url_base != None and args.keep_path == None:
+                auto_find_directory(url=args.autourl,auth_type=args.auth_type,token=args.token,user_set_base=args.url_base)
+            if args.url_base == None and args.keep_path != None:
+                auto_find_directory(url=args.autourl,auth_type=args.auth_type,token=args.token,keep_path=args.keep_path)
+            else:
+                auto_find_directory(url=args.autourl,auth_type=args.auth_type,token=args.token)
+        elif args.single_js:
+            if args.url_base == None:
+                get_apis_from_js_link(js_link=args.single_js,token=args.token,auth_type=args.auth_type,warning=True)
+            else:
+               get_apis_from_js_link(js_link=args.single_js,user_set_base=args.url_base,token=args.token,auth_type=args.auth_type,warning=True) 
+        fuzzing_complete()
+    except KeyboardInterrupt:
+        print("\n")
+        print(colored("[!] USER EXITED\n","green"))
+        
+
